14322394f3d5e2bcd3dfa368c785cb17952da50d
sujensen <susanjensen@fastmail.fm>
2024-06-18 22:04:22 -0700

    use more operator args, and add fact table operator

--- dags/final_project.py
+++ dags/final_project.py
@@ -6,7 +6,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from operators import (StageToRedshiftOperator, LoadFactOperator,
                        LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
+from helpers.sql_queries import SqlQueries
 
 default_args = {
     'owner': 'udacity',
@@ -29,18 +29,30 @@ def final_project(**kwargs):
 
     stage_events_to_redshift = StageToRedshiftOperator(
         task_id='Stage_events',
+        table='staging_events',
+        s3_key='log-data/{{ execution_date.year }}/{{ \'%02d\' % execution_date.month }}/{{ ds }}-events.json',
+        s3_bucket='susan-airflow-bucket-3',
+        redshift_conn_id='redshift',
+        aws_credentials_id='aws_credentials',
+        json_format='s3://susan-airflow-bucket-3/log_json_path.json',
     )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
         task_id='Stage_songs',
+        table='staging_songs',
+        s3_key='song-data/A',
+        s3_bucket='susan-airflow-bucket-3',
+        redshift_conn_id='redshift',
+        aws_credentials_id='aws_credentials',
     )
 
-
-    """
     load_songplays_table = LoadFactOperator(
         task_id='Load_songplays_fact_table',
+        sql_statement=SqlQueries.songplay_table_insert,
+        target_db='dev',
+        target_table='public.songplays',
     )
-
+    """
     load_user_dimension_table = LoadDimensionOperator(
         task_id='Load_user_dim_table',
     )
@@ -66,7 +78,7 @@ def final_project(**kwargs):
 
     # Add dependencies to the graph
     start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
-    #[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+    [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
     #load_songplays_table >> [load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table]
     #[load_song_dimension_table, load_user_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks
     #run_quality_checks >> end_operator
--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,28 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+from helpers.sql_queries import SqlQueries
+
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                 *args,
+                 task_id: str = "foo",
+                 sql_statement: str = "my_sql",
+                 target_db: str = "my_target_db",
+                 target_table: str = "my_target_table",
+                 **kwargs):
 
-        super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        super(LoadFactOperator, self).__init__(*args, task_id=task_id, **kwargs)
+        self.task_id = task_id
+        self.sql_statement = sql_statement
+        self.target_db = target_db
+        self.target_table = target_table
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift_hook = PostgresHook("redshift")
+        insert_statement = "insert into {0}.{1} ({2})".format(self.target_db, self.target_table, self.sql_statement)
+        redshift_hook.run(insert_statement)
